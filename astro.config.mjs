1d755c3a7c914c834a1bac2478bd973a7064e218
Sprey.Win <vitled@gmail.com>
2026-08-29 20:40:51 +0500

    fix: deploy docs from custom domain root

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
-  site: 'https://spreywin.github.io',
-  base: '/sprey-docs',
+  site: 'https://docs.sprey.win',
   integrations: [
     starlight({
       title: 'Sprey Docs',
